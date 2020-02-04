46602f9a260da2074d1e2ed79fcb2a7d088712d9
lastrites2018 <lastrites@nate.com>
2020-02-04 11:52:07 +0900

    Add ui-kitten

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 // import styled from 'styled-components/native'; // Styled 대문자일 경우에는 IDE 적용 안 됨;
+
+import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
+import {mapping, light as lightTheme} from '@eva-design/eva';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
+
 import Navigator from '~/Components/Navigator';
 import {ValueTimerContextProvider} from '~/Context/ValueTimerContext';
 
@@ -7,9 +12,14 @@ interface Props {}
 
 const App: React.FC = ({}: Props) => {
   return (
-    <ValueTimerContextProvider>
-      <Navigator />
-    </ValueTimerContextProvider>
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider mapping={mapping} theme={lightTheme}>
+        <ValueTimerContextProvider>
+          <Navigator />
+        </ValueTimerContextProvider>
+      </ApplicationProvider>
+    </>
   );
 };
 
--- src/Components/Navigator.tsx
+++ src/Components/Navigator.tsx
@@ -1,16 +1,55 @@
-import {createAppContainer} from 'react-navigation';
+import {createAppContainer, SafeAreaView} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
+
+import React from 'react';
+import {
+  BottomNavigation,
+  BottomNavigationTab,
+  Icon,
+  IconElement,
+} from '@ui-kitten/components';
+import {ImageStyle} from 'react-native';
 
 import Timer from '~/Components/Timer';
 import History from '~/Components/History';
 
-const TabNavigator = createBottomTabNavigator({
-  Home: {
-    screen: Timer,
+const ClockIcon = (style: ImageStyle): IconElement => (
+  <Icon {...style} name="clock-outline" />
+);
+const ListIcon = (style: ImageStyle): IconElement => (
+  <Icon {...style} name="list-outline" />
+);
+
+const TabBarComponent = ({navigation}) => {
+  const onSelect = index => {
+    const selectedTabRoute = navigation.state.routes[index];
+    navigation.navigate(selectedTabRoute.routeName);
+  };
+
+  return (
+    <SafeAreaView>
+      <BottomNavigation
+        selectedIndex={navigation.state.index}
+        onSelect={onSelect}>
+        <BottomNavigationTab icon={ClockIcon} />
+        <BottomNavigationTab icon={ListIcon} />
+      </BottomNavigation>
+    </SafeAreaView>
+  );
+};
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Timer: {
+      screen: Timer,
+    },
+    History: {
+      screen: History,
+    },
   },
-  Test: {
-    screen: History,
+  {
+    tabBarComponent: TabBarComponent,
   },
-});
+);
 
 export default createAppContainer(TabNavigator);
