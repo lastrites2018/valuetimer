c210b1063bd1730a21bb0d400ea4a8b891895a92
lastrites2018 <lastrites@nate.com>
2020-02-06 01:44:15 +0900

    Add date-fns & Change style

--- src/Components/History.tsx
+++ src/Components/History.tsx
@@ -1,76 +1,47 @@
-import React, {useState, useEffect, useContext} from 'react';
-import {StyleSheet, Text, View, Platform, FlatList} from 'react-native';
+import React, {useContext} from 'react';
+import {Text, View, FlatList, StyleSheet} from 'react-native';
 import styled from 'styled-components/native';
 import {ValueTimerContext} from '~/Context/ValueTimerContext';
 
 import {getRemaining, numberWithCommas} from '~/util/index';
+
+import format from 'date-fns/format';
+import koLocale from 'date-fns/locale/ko';
 
 const History: React.FC = () => {
   const {history} = useContext(ValueTimerContext);
 
   return (
     <Container>
+      <Header>Timeline</Header>
       {history.length === 0 && (
-        <View
-          style={{
-            flex: 1,
-            // marginTop: 150,
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'row',
-          }}>
+        <NoData>
           <Text style={{fontSize: 30}}>아직 데이터가 없습니다.</Text>
-        </View>
+        </NoData>
       )}
       <FlatList
-        style={{marginTop: 200}}
         data={history}
+        keyExtractor={item => {
+          return `historyList-${item.id}`;
+        }}
         renderItem={({item}) => {
           const {hours, mins, secs} = getRemaining(item.time);
 
-          const DisplayTime = () => {
-            let displaySecond: string = secs;
-            if (secs[0] === '0') {
-              displaySecond = secs.slice(1);
-            }
-
-            if (hours !== '00') {
-              return `${hours}시간 ${mins}분 ${displaySecond}초`;
-            } else if (hours === '00' && mins !== '00') {
-              return `${mins}분 ${displaySecond}초`;
-            } else {
-              return `${displaySecond}초`;
-            }
-          };
-
           let amountType: string = item.amount >= 0 ? 'positive' : 'negative';
+          const timeDisplay = displayTime(hours, mins, secs);
 
           return (
-            <View
-              style={{
-                flex: 1,
-                marginBottom: 10,
-                margin: 20,
-                alignItems: 'center',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-              }}>
+            <View style={styles.listView}>
               <View>
-                <List amountType={amountType}>
-                  {new Date(item.start_date).toLocaleString('ko-KR')}
-                </List>
-                <List amountType={amountType}>
-                  {new Date(item.end_date).toLocaleString('ko-KR')}
-                </List>
+                <Text>{dateOnly(item.start_date)}</Text>
+                <Text>{dateOnly(item.end_date)}</Text>
               </View>
               <View>
-                <List amountType={amountType}>{DisplayTime()}</List>
-                <View>
-                  <List amountType={amountType}>
-                    시간당 {numberWithCommas(item.hourly_rate)}
-                    {item.currency}
-                  </List>
-                </View>
+                <Text>{timeDisplay}</Text>
+                <List amountType={amountType}>
+                  시간당 {numberWithCommas(item.hourly_rate)}
+                  {item.currency}
+                </List>
               </View>
 
               <List amountType={amountType}>
@@ -85,9 +56,47 @@ const History: React.FC = () => {
   );
 };
 
+const styles = StyleSheet.create({
+  listView: {
+    flex: 1,
+    margin: 20,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
+
 const Container = styled.View`
   flex: 1;
+  margin-top: 50px;
 `;
+
+const NoData = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+const dateOnly = (date: number): string => {
+  if (!date) return '';
+  return format(new Date(date), 'yyyy-MM-dd HH:mm:ss', {locale: koLocale});
+};
+
+const displayTime = (hours: string, mins: string, secs: string) => {
+  let displaySecond: string = secs;
+  if (secs[0] === '0') {
+    displaySecond = secs.slice(1);
+  }
+
+  if (hours !== '00') {
+    return `${hours}시간 ${mins}분 ${displaySecond}초`;
+  } else if (hours === '00' && mins !== '00') {
+    return `${mins}분 ${displaySecond}초`;
+  } else {
+    return `${displaySecond}초`;
+  }
+};
 
 interface IAmountType {
   readonly amountType?: string;
@@ -99,6 +108,10 @@ const List = styled.Text<IAmountType>`
     else if (props.amountType === 'negative') return '#f03e3e';
     else return '#000';
   }};
+`;
+
+const Header = styled.Text`
+  text-align: center;
 `;
 
 export default History;
