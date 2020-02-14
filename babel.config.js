2e2c2c758d4f4b2e8d21efc59cd4752fe3a7152d
lastrites2018 <lastrites@nate.com>
2020-02-14 18:34:55 +0900

    Update babel config to make the change according to env

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,28 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
+module.exports = api => {
+  const babelEnv = api.env();
+  const plugins = [];
+  //change to 'production' to check if this is working in 'development' mode
+  if (babelEnv !== 'development') {
+    plugins.push(['transform-remove-console', {exclude: ['error', 'warn']}]);
+    plugins.push([
       'babel-plugin-root-import',
       {
         rootPathPrefix: '~',
         rootPathSuffix: 'src',
       },
-    ],
-    ['transform-remove-console'],
-  ],
+    ]);
+  } else {
+    plugins.push([
+      'babel-plugin-root-import',
+      {
+        rootPathPrefix: '~',
+        rootPathSuffix: 'src',
+      },
+    ]);
+  }
+
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins,
+  };
 };
